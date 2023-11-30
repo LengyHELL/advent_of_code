89973a444565ac74597d1e3f9717317191f314b9
LengyHELL <ubi19998@gmail.com>
2023-11-30 20:22:39 +0100

    Initial commit

---
+++ day1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string message = "hello";
+
+  for (int i = 0; i < 10; i++) {
+    std::cout << message << std::endl;
+  }
+
+  return 0;
+}
