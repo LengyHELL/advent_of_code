10b9f79c9978569bda4b3c215b7db992c71daee1
LengyHELL <ubi19998@gmail.com>
2023-12-09 14:32:12 +0100

    Completing Day 9

---
+++ day9/day9.py
@@ -0,0 +1,33 @@
+"""
+Advent of Code Day 8
+"""
+
+import sys
+
+with open(sys.argv[1], encoding="utf-8") as file:
+    lines = file.read().split("\n")
+
+    previous_total = 0
+    next_total = 0
+    for line in lines:
+        serials = [[int(number) for number in line.split(" ")]]
+
+        while any(number != 0 for number in serials[-1]):
+            serials.append(
+                [
+                    serials[-1][index] - serials[-1][index - 1]
+                    for index in range(1, len(serials[-1]))
+                ]
+            )
+
+        previous_number = 0
+        next_number = 0
+        for serial in serials[::-1]:
+            previous_number = serial[0] - previous_number
+            next_number = serial[-1] + next_number
+
+        previous_total += previous_number
+        next_total += next_number
+
+    print("The sum of the extrapolated numbers for part 1 is:", next_total)
+    print("The sum of the extrapolated numbers for part 2 is:", previous_total)
