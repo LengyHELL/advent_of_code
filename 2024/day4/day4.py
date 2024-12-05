3dc037a17e0a1ff75515a338b568fdcf39075276
LengyHELL <ubi19998@gmail.com>
2024-12-05 01:22:41 +0100

    Adding modules

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -3,40 +3,7 @@ Advent of Code Day 4
 """
 
 import sys
-
-
-class Coord:
-    """
-    Represents a 2d coordinate
-    """
-
-    x: int
-    y: int
-
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x},{self.y})"
-
-    def __repr__(self):
-        return f"({self.x},{self.y})"
-
-    def __add__(self, other):
-        return Coord(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Coord(self.x - other.x, self.y - other.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def copy(self):
-        return Coord(self.x, self.y)
+from modules.coord import Coord
 
 
 class Grid:
---
+++ modules/coord.py
@@ -0,0 +1,47 @@
+class Coord:
+    x: int
+    y: int
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+    def __add__(self, other: "Coord"):
+        return Coord(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Coord"):
+        return Coord(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other: "Coord"):
+        return self.x == other.x and self.y == other.y
+
+    def __mul__(self, number: int):
+        return Coord(self.x * number, self.y * number)
+
+    __rmul__ = __mul__
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def copy(self):
+        return Coord(self.x, self.y)
+
+    def distance(self, other: "Coord"):
+        return abs(other.x - self.x) + abs(other.y - self.y)
+
+    def rotate(self, clockwise=True):
+        x, y = self.y, self.x
+        if clockwise:
+            x *= -1
+        else:
+            y *= -1
+        return Coord(x, y)
